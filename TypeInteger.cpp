5857c638367bf0ffb0a83dc61fcff530bb95231a
Bruna <70817817+brunacaveion@users.noreply.github.com>
2022-06-20 18:51:49 -0300

    Add files via upload

---
+++ TypeInteger.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int Numero;
+	Numero = 45;
+	std::cout << "Valor Numero: " << Numero << std::endl;
+	std::cout << "Tamanho da Variavel Numero: " << sizeof(Numero)<< "Bytes" << "\n";
+	std::cout << "Endereco Carregado na Memoria: " << &Numero << "\n";
+
+	system("PAUSE");
+
+}
